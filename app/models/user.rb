bf58fef091887fe0db63dd83e1bcf93edbea4100
Howard Liu <howard@ixnet.work>
2022-04-11 17:55:42 +0800

    Add account activation emails (11.2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,5 +50,7 @@ class User < ApplicationRecord
 
   def create_activation_digest
     # Create the token and digest.
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
