3437ba458f44c1ad81bf20f3c060c4f106508ae5
Howard Liu <howard@ixnet.work>
2022-04-11 23:11:58 +0800

    Add profile micropost list and basic feed (13.2-3)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
     # Only show profile for activated users
     # TODO: test required
     redirect_to root_url and return unless @user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
@@ -59,14 +60,6 @@ class UsersController < ApplicationController
   end
 
   # Before filters
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to(login_url)
-    end
-  end
 
   # Confirms the correct user.
   def correct_user
