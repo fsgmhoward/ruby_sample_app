5f071c890bd22a01421a3245dbc3285887aeb3fa
Howard Liu <howard@ixnet.work>
2022-04-11 23:54:58 +0800

    Add image uploading facility (13.4)
    - Note: Azure is used instead of AWS

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,9 @@
+# Unlike tutorial, Azure is used for the sake of convenience
+CarrierWave.configure do |config|
+  config.azure_storage_account_name = ENV["AZURE_STORAGE_ACCOUNT_NAME"]
+  config.azure_storage_access_key = ENV["AZURE_STORAGE_ACCESS_KEY"]
+  # config.azure_storage_blob_host = "YOUR STORAGE BLOB HOST" # optional
+  config.azure_container = ENV["AZURE_STORAGE_CONTAINER"]
+  # config.asset_host = "YOUR CDN HOST" # optional
+  config.auto_sign_urls = true
+end
