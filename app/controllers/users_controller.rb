e5a34678de913fb958531bc4f084204bd709baaa
Howard Liu <howard@ixnet.work>
2022-04-11 19:43:27 +0800

    Refine activation mechanism (11.3)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,9 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    # Only show activated users
+    # TODO: test required
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def new
@@ -13,12 +15,15 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    # Only show profile for activated users
+    # TODO: test required
+    redirect_to root_url and return unless @user.activated?
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
