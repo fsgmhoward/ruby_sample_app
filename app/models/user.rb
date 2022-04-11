ced5a17ec5997141ba4f29508bf681a8f0156405
Howard Liu <howard@ixnet.work>
2022-04-11 17:32:42 +0800

    Add account activation resource (11.1)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -36,5 +38,17 @@ class User < ApplicationRecord
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  # Converts email to all lower-case.
+  def downcase_email
+    # TODO: Existing test suite cannot test this!
+    self.email.downcase!
+  end
+
+  def create_activation_digest
+    # Create the token and digest.
   end
 end
